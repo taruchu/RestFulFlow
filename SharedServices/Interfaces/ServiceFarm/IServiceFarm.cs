a7d1018d8adee96f5cb208e9ee9ec8cb46ccb1d1
taruchu <tmccullough.softwaredeveloper@gmail.com>
2018-07-16 12:59:10 -0700

    Adding Service Farm interfaces.

---
+++ SharedServices/Interfaces/ServiceFarm/IServiceFarm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedServices.Interfaces.ServiceFarm
+{
+    public interface IServiceFarm : IDisposable
+    {
+        bool CompositionRoute(); //NOTE: New up all objects, associate them to a router, and establish their composition route.
+        List<IServiceFarmServiceBase> ServiceList { get; set; }
+
+    }
+}
---
+++ SharedServices/Interfaces/ServiceFarm/IServiceFarmServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using SharedServices.Services.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedServices.Interfaces.ServiceFarm
+{
+    public interface IServiceFarmServiceBase : IDisposable
+    {
+        MessageBus<string> MessageBus { get; set; }
+        MessageBusReaderBank<string> MessageBusReaderBank { get; set; }
+        Action<string> HandleMessageFromRouter { get; set; }
+        bool Put(string serviceUrl, string response);
+    }
+}
